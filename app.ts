4cf58e4173df5643091c09a5717b12e38145f4e2
JustinCockrell1 <69552295+JustinCockrell1@users.noreply.github.com>
2026-02-08 10:22:27 -0600

    add sound

--- app.ts
+++ app.ts
@@ -1,7 +1,8 @@
 console.log("hello");
 const test = new WebSocket("wss://hot-potato-42gu.onrender.com")
 const body = document.getElementById("body")!
-
+const audio = new Audio("https://actions.google.com/sounds/v1/cartoon/wood_plank_flicks.ogg");
+let hasInteracted = false;
 let myPosition = -1;
 
 test.onopen = (ev) =>{
@@ -18,6 +19,8 @@ test.onmessage = (ev)=>{
     else if(data.type === "current") {
         if(data.currentTurn === myPosition) {
             body.style.backgroundColor = "red"
+            if(hasInteracted)
+            audio.play();
         } else {
             body.style.background = "white"
         }
@@ -32,6 +35,7 @@ test.onmessage = (ev)=>{
 console.log(body);
 
 body?.addEventListener("click", (ev)=>{
+    hasInteracted = true;
     console.log("click", ev.clientX);
     if(ev.clientX < window.innerWidth/2)
     test.send("left")
